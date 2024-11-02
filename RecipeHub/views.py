49838985b255018bfd8656fb8ced544772cb373d
andrrreeeasbro <andrrreeeas@bk.ru>
2024-11-02 18:46:42 +0300

    do template

--- RecipeHub/views.py
+++ RecipeHub/views.py
@@ -7,9 +7,9 @@ from django.http import Http404
 # представление для всех рецептов
 def recipe_list(request):
     recipes = Post_recipe.objects.all()
-    return render(request, 'templates/base.html', {'recipes': recipes})
+    return render(request, 'index.html', {'recipes': recipes})
     
 # представление для одного рецепта
 def recipe_details(request, name):
     recipe = get_object_or_404(Post_recipe.recipe_manager, name=name)
-    return render(request, 'templates/recipe_detail.html', {'recipe':recipe})
+    return render(request, 'recipes/recipe_detail.html', {'recipe':recipe})
